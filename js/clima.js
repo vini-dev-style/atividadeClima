d312220b3445a659458775be4ea9a54b52e482a2
Vinícius Sanches <sanchnini07@gmail.com>
2026-08-20 09:08:31 -0300

    site de clima

---
+++ js/clima.js
@@ -0,0 +1,68 @@
+const botaoBuscar = document.getElementById("buscar");
+
+botaoBuscar.addEventListener("click", buscarClima);
+
+function buscarClima() {
+
+    const cidade = document.getElementById("cidade").value.trim();
+
+    if (cidade === "") {
+        alert("Digite o nome de uma cidade.");
+        return;
+    }
+
+    const chaveApi = "df7622edbc8c4800b6f1a2f870860829";
+
+    const url =
+        `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chaveApi}&units=metric&lang=pt_br`;
+
+    fetch(url)
+
+        .then(function(resposta) {
+
+            if (!resposta.ok) {
+                throw new Error("Cidade não encontrada.");
+            }
+
+            return resposta.json();
+        })
+
+        .then(function(dados) {
+
+            console.log(dados);
+
+            document.getElementById("nomeCidade").textContent =
+                dados.name;
+
+            document.getElementById("temp-min").textContent =
+                dados.main.temp_min.toFixed(1);
+
+                document.getElementById("temp-max").textContent =
+                dados.main.temp_max.toFixed(1);
+
+            document.getElementById("sensa-min").textContent =
+                dados.main.feels_like.toFixed(1);
+
+            document.getElementById("sensa-max").textContent =
+                dados.main.feels_like.toFixed(1);
+
+
+            document.getElementById("umida-min").textContent =
+                dados.main.humidity;
+            
+            document.getElementById("umida-max").textContent =
+                dados.main.humidity;
+
+            const ventoKmH = dados.wind.speed * 3.6;
+
+            document.getElementById("vento-valor").textContent =
+                ventoKmH.toFixed(1);
+        })
+
+        .catch(function(erro) {
+
+            console.log("Erro:", erro);
+
+            alert("Não foi possível encontrar a cidade.");
+        });
+}
